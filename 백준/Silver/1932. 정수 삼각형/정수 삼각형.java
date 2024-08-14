f1a07e754c7d9559fff02144a47e517d036e55eb
조재현 <jaehyun_0103@naver.com>
2024-08-15 02:57:05 +0900

    [Silver II] Title: 정수 삼각형, Time: 232 ms, Memory: 26952 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[][] triangle = new int[n][n];
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < i + 1; j++) {
+                triangle[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        System.arraycopy(triangle[n - 1], 0, dp[n - 1], 0, n);
+        
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[i][j] = Math.max(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j];
+            }
+        }
+
+        System.out.println(dp[0][0]);
+    }
+}
