2c11ea5d0c1d12033ffc391eb3aabe868c26a8c5
조재현 <jaehyun_0103@naver.com>
2024-08-19 00:33:03 +0900

    [Silver III] Title: 소수 구하기, Time: 708 ms, Memory: 29972 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] tokens = br.readLine().split(" ");
+        int M = Integer.parseInt(tokens[0]);
+        int N = Integer.parseInt(tokens[1]);
+
+        for (int i = M; i <= N; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        if (num <= 3) {
+            return true;
+        }
+        if (num % 2 == 0 || num % 3 == 0) {
+            return false;
+        }
+        for (int i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
