fd0f34a112eb5348f94a96dd4b197ce231cf919f
조재현 <jaehyun_0103@naver.com>
2024-08-22 21:05:33 +0900

    [Silver II] Title: 베르트랑 공준, Time: 328 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -1,21 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
         int n;
         while (true) {
-            n = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(br.readLine());
             if (n == 0)
                 break;
-
-            st = new StringTokenizer(br.readLine());
 
             int count = 0;
             for (int i = n + 1; i <= 2 * n; i++) {
