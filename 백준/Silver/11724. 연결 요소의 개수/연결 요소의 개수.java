6bc42c7d012f06e65c82a5b79a2af645181d5c8e
조재현 <jaehyun_0103@naver.com>
2024-08-24 21:19:02 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 472 ms, Memory: 116548 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-    static ArrayList<Integer>[] graph;
+    static int[][] graph;
     static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
@@ -15,40 +15,36 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        graph = new ArrayList[N + 1];
+        graph = new int[N + 1][N + 1];
         visited = new boolean[N + 1];
-
-        for (int i = 1; i <= N; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
+
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            graph[u].add(v);
-            graph[v].add(u);
+
+            graph[u][v] = graph[v][u] = 1;
         }
 
-        int connectedComponents = 0;
+        int cnt = 0;
 
         for (int i = 1; i <= N; i++) {
             if (!visited[i]) {
                 dfs(i);
-                connectedComponents++;
+                cnt++;
             }
         }
 
-        System.out.println(connectedComponents);
+        System.out.println(cnt);
     }
 
-    static void dfs(int node) {
-        visited[node] = true;
+    static void dfs(int x) {
+        visited[x] = true;
 
-        for (int neighbor : graph[node]) {
-            if (!visited[neighbor]) {
-                dfs(neighbor);
+        for (int i = 1; i < graph.length; i++) {
+            if (graph[x][i] == 1 && !visited[i]) {
+                dfs(i);
             }
         }
     }
