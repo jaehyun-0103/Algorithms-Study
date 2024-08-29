0154ab05637f62f46dae6bf62e178a3f44a4f63e
조재현 <jaehyun_0103@naver.com>
2024-08-30 00:06:12 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 104 ms, Memory: 16492 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[] numbers;
+    static int[] operators = new int[4];
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        numbers = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 4; i++) {
+            operators[i] = Integer.parseInt(st.nextToken());
+        }
+
+        calculate(numbers[0], 1);
+
+        System.out.println(max);
+        System.out.println(min);
+    }
+
+    static void calculate(int currentResult, int index) {
+        if (index == N) {
+            max = Math.max(max, currentResult);
+            min = Math.min(min, currentResult);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            if (operators[i] > 0) {
+                operators[i]--;
+                switch (i) {
+                    case 0:
+                        calculate(currentResult + numbers[index], index + 1);
+                        break;
+                    case 1:
+                        calculate(currentResult - numbers[index], index + 1);
+                        break;
+                    case 2:
+                        calculate(currentResult * numbers[index], index + 1);
+                        break;
+                    case 3:
+                        calculate(currentResult / numbers[index], index + 1);
+                        break;
+                }
+                operators[i]++;
+            }
+        }
+    }
+}
