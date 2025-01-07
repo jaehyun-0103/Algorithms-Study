02a0732929d7c833c06e6d10601520000e0b79f3
조재현 <jaehyun_0103@naver.com>
2025-01-07 14:56:07 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 276 ms, Memory: 31844 KB -BaekjoonHub

--- 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.java
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
@@ -8,23 +6,27 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N=Integer.parseInt(st.nextToken());
         String game=st.nextToken();
         Set<String> names = new HashSet<>();
-        
+
         for(int i=0;i<N;i++){
             st = new StringTokenizer(br.readLine());
             String name = st.nextToken();
             names.add(name);
         }
 
-        if(game.equals("Y"))
-            System.out.println(names.size());
-        else if(game.equals("F"))
-            System.out.println(names.size()/2);
-        else if(game.equals("O"))
-            System.out.println(names.size()/3);
+        if (game.equals("Y"))
+            bw.write(String.valueOf(names.size()) + "\n");
+        else if (game.equals("F"))
+            bw.write(String.valueOf(names.size() / 2) + "\n");
+        else if (game.equals("O"))
+            bw.write(String.valueOf(names.size() / 3) + "\n");
+
+        bw.flush();
+        bw.close();
     }
 }
