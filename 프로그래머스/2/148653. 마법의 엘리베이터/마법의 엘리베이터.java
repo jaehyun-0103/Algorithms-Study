f12828bf59482df2ef1acf8ef2e0df9d3f927ba7
조재현 <jaehyun_0103@naver.com>
2025-01-10 00:04:17 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 86.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int storey) {
+        int answer = 0;
+        
+        while(storey>0){
+            int num=storey%10;
+            storey/=10;
+            
+            if(num==5){
+                if(storey%10<5)
+                    answer+=num;
+                else{
+                    answer+=(10-num);
+                    storey+=1;
+                }
+                    
+            } else if(num<5)
+                answer+=num;
+            else{
+                answer+=(10-num);
+                storey+=1;
+            }
+        }
+        
+        return answer;
+    }
+}
