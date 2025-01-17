52f48f2ab2eb22e85130df98da2657699a0f442b
조재현 <jaehyun_0103@naver.com>
2025-01-17 21:41:24 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 23.31 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        String[] nums=s.split(" ");
+        Arrays.sort(nums, (a, b)->Integer.parseInt(a)-Integer.parseInt(b));
+        
+        answer=answer+nums[0]+" "+nums[nums.length-1];
+        
+        return answer;
+    }
+}
