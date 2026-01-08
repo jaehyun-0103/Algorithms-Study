4aab8197bdb75c75277700be446ecd0570eca5af
조재현 <jaehyun_0103@naver.com>
2026-01-08 13:18:30 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.06 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -4,31 +4,20 @@ class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
         HashMap<String, Integer> hm=new HashMap<>();
-        int order=1;
         hm.put(words[0], 0);
         
         for(int i=1; i<words.length; i++){
-            char preLast, postFirst;
-            preLast=words[i-1].charAt(words[i-1].length()-1);
-            postFirst=words[i].charAt(0);
+            char preLast = words[i-1].charAt(words[i-1].length()-1);
+            char postFirst = words[i].charAt(0);
             
-            if(i%n==0){
-                order++;
-            }
-            
-            if(hm.containsKey(words[i])){
-                answer[0]=i%n+1;
-                answer[1]=order;
+            if(hm.containsKey(words[i]) || preLast!=postFirst){
+                answer[0] = (i%n) + 1;
+                answer[1] = (i/n) + 1;
                 
-                break;
-            } else if(preLast!=postFirst){
-                answer[0]=i%n+1;
-                answer[1]=order;
-                    
-                break;
-            } else{
-                hm.put(words[i], i);
+                return answer;
             }
+            
+            hm.put(words[i], i);
         }
 
         return answer;
