54f2aef4871cd117733cea720e79174d9b153db8
조재현 <jaehyun_0103@naver.com>
2026-01-15 10:19:01 +0900

    [level 2] Title: H-Index, Time: 0.65 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer=1;
+        
+        Arrays.sort(citations);
+        
+        for(int i=citations.length-1; i>=0; i--){
+            if(citations[i]>=answer)
+                answer++;
+            else
+                break;
+        }
+        return answer-1;
+    }
+}
