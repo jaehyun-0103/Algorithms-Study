6be9604acfa006258666146810160270851a5e02
조재현 <jaehyun_0103@naver.com>
2026-01-20 09:23:23 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.07 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        int len=s.length();
+        
+        if(len!=4 && len!=6)
+            return false;
+        
+        for(int i=0; i<len; i++){
+            if(!Character.isDigit(s.charAt(i)))
+                return false;
+                
+        }
+        
+        return answer;
+    }
+}
